5631a555e5dd47f89a89c78225e21000392849ec
lissy <slissy513@gmail.com>
2021-08-24 18:17:22 -0400

    completed 'signup form' file

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
+import { CREATE_USER } from '../utils/mutations'
 
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+
+  const [createUser] = useMutation(CREATE_USER);
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
@@ -28,18 +31,16 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await createUser({
+      variables: { ...userFormData},
+    });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+    Auth.login(data.addUser.token);
+
     } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+    console.error(err);
+    setShowAlert(true);
     }
 
     setUserFormData({

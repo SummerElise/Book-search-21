4ff81bfffb69e77ab0abe2c9303f2011f7315697
lissy <slissy513@gmail.com>
2021-08-24 17:17:49 -0400

    added 'queries.js'file and coded it

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import gql from 'graphql-tag';
+
+export const GET_ME = gql`
+{
+    me{
+    _id
+    username
+    email
+    bookCount
+    savedBook {
+        bookId
+        authors
+        title
+        description
+        link
+        image
+        }
+    }
+}
+
+`;
